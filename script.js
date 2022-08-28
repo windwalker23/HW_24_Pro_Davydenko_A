992e939eb00bb3046383d6fd614ae6e08560ace7
unknown <yeskennyt@gmail.com>
2022-08-28 15:47:15 +0300

    v1.0

---
+++ script.js
@@ -0,0 +1,38 @@
+const userName = document.getElementById("userName");
+const form = document.getElementById("searchForm");
+const container = document.getElementById("container");
+const API = "https://api.github.com/users/";
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    controller(userName.value)
+        .then(data => renderCard(data))
+        .catch(err => console.log(err))
+})
+
+const controller = async userName => {
+    const response = await fetch(API + userName);
+    if(response.status > 399) {
+        throw new Error(alert(`User with ${userName} login not found`))
+    } else {
+        return response.json();
+    }
+}
+
+function renderCard(data) {
+    const card = document.createElement("div");
+    const avatar = document.createElement("img");
+    const userName = document.createElement("span");
+    const follow = document.createElement("p")
+
+    card.classList.add("card");
+
+    card.append(avatar);
+    card.append(userName);
+    card.append(follow);
+    container.append(card)
+
+    avatar.src = data.avatar_url;
+    userName.textContent = data.login;
+    follow.textContent = `${data.followers} followers · ${data.following} following`
+}
